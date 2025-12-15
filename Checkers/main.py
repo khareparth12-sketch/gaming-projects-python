78b69447e7d7fd7ba86de76798bde071950e5ac8
ParthKhare21 <khare.parth2112@gmail.com>
2025-12-15 23:36:09 +0530

    Modified Checkers project

--- Checkers/main.py
+++ Checkers/main.py
@@ -1,2 +1,19 @@
 import pygame
+import time
+import random
+from checkers.constant import WIDTH, HEIGHT
 
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Dodge")
+
+def main():
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
