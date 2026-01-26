05d84355b398d83fd941b9b8784e3b4008cce437
ParthKhare21 <khare.parth2112@gmail.com>
2026-01-26 17:10:28 +0530

    Modified Platformer Game

--- Platformer/main.py
+++ Platformer/main.py
@@ -7,3 +7,31 @@ from os.path import isfile, join
 
 pygame.init()
 
+pygame.display.set_caption("Platformer Game")
+
+BACKGROUND = (144, 201, 120)
+WIDTH, HEIGHT = 900, 700
+FPS = 60
+PLAYER_VEL = 5
+# GRAVITY = 1
+# JUMP_VEL = 15
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def main(screen):
+    clock = pygame.time.Clock()
+
+    run = True
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+    pygame.quit()
+    quit()      
+
+if __name__ == "__main__":
+    main(screen)
