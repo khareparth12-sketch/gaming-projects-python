c1fe0653f5bdc8d70c82fbf5d2ff55aa54d868d7
ParthKhare21 <khare.parth2112@gmail.com>
2026-01-27 17:59:22 +0530

    Started setting the game environment

--- Platformer/main.py
+++ Platformer/main.py
@@ -9,7 +9,6 @@ pygame.init()
 
 pygame.display.set_caption("Platformer Game")
 
-BACKGROUND = (144, 201, 120)
 WIDTH, HEIGHT = 900, 700
 FPS = 60
 PLAYER_VEL = 5
@@ -18,8 +17,62 @@ PLAYER_VEL = 5
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
+class Player(pygame.sprite.Sprite):
+    COLOR = (255, 0, 0)
+
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = None
+        self.direction = "left"
+        self.animation_count = 0
+
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+    def move_left(self, vel):
+        self.x_vel = -vel
+        self.direction = "left"
+        self.animation_count = 0
+
+    def move_right(self, vel):
+        self.x_vel = vel
+        self.direction = "right"
+        self.animation_count = 0
+
+    def loop(self, fps):
+        self.move(self.x_vel, self.y_vel)
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.COLOR, self.rect)
+        
+def get_bg(name):
+    image = pygame.image.load(join("assets", "Background", name))
+    _, _, w, h = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH//w +1):
+        for j in range(HEIGHT//h +1):
+            pos = (i*w, j*h)
+            tiles.append(pos)
+
+    return tiles, image
+
+def draw(screen, bg, bg_img, player):
+    for tile in bg:
+        screen.blit(bg_img, tile)
+
+    player.draw(screen)
+
+    pygame.display.update()
+
 def main(screen):
     clock = pygame.time.Clock()
+    bg, bg_img = get_bg("Brown.png")
+
+    player = Player(100, 100, 50, 50)
 
     run = True
     while run:
@@ -29,6 +82,8 @@ def main(screen):
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        draw(screen, bg, bg_img, player)
 
     pygame.quit()
     quit()      
