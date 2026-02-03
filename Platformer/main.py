4fb12d9602b99ba920ae17c055a504863d2fef1d
ParthKhare21 <khare.parth2112@gmail.com>
2026-02-03 23:34:23 +0530

    Defined Jumping. Jumping Bug

--- Platformer/main.py
+++ Platformer/main.py
@@ -56,7 +56,7 @@ class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0)
     GRAVITY = 1
     SPRITES = load_sprite_sheets("MainCharacters", "VirtualGuy", 32, 32, True)
-    ANIMATION_DELAY = 4
+    ANIMATION_DELAY = 3
 
     def __init__(self, x, y, width, height):
         super().__init__()
@@ -67,6 +67,14 @@ class Player(pygame.sprite.Sprite):
         self.direction = "left"
         self.animation_count = 0
         self.fall_count = 0
+        self.jump_count = 0
+
+    def jump(self):
+        self.y_vel = -self.GRAVITY * 8
+        self.animation_count = 0
+        self.jump_count += 1
+        if self.jump_count == 1:
+            self.fall_count = 0
 
     def move(self, dx, dy):
         self.rect.x += dx
@@ -74,16 +82,18 @@ class Player(pygame.sprite.Sprite):
 
     def move_left(self, vel):
         self.x_vel = -vel
-        self.direction = "left"
-        self.animation_count = 0
+        if self.direction != "left":
+            self.direction = "left"
+            self.animation_count = 0
 
     def move_right(self, vel):
         self.x_vel = vel
-        self.direction = "right"
-        self.animation_count = 0
+        if self.direction != "right":
+            self.direction = "right"
+            self.animation_count = 0
 
     def loop(self, fps):
-        self.y_vel = min(1, (self.fall_count/fps)*self.GRAVITY)
+        self.y_vel += min(1, (self.fall_count/fps) * self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
 
         self.fall_count += 1
@@ -201,6 +211,10 @@ def main(screen):
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and player.jump_count < 2:
+                    player.jump()
 
         player.loop(FPS)
         handle_movement(player, floor)
