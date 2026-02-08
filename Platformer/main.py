678391bee1493d44fce00502dc399d329b7483a8
ParthKhare21 <khare.parth2112@gmail.com>
2026-02-08 22:35:13 +0530

    Added Fire collison.All components made. Still need to set the game environment

--- Platformer/main.py
+++ Platformer/main.py
@@ -67,6 +67,8 @@ class Player(pygame.sprite.Sprite):
         self.animation_count = 0
         self.fall_count = 0
         self.jump_count = 0
+        self.hit = False
+        self.hit_count = 0
 
     def jump(self):
         self.y_vel = -self.GRAVITY * 8
@@ -78,6 +80,10 @@ class Player(pygame.sprite.Sprite):
     def move(self, dx, dy):
         self.rect.x += dx
         self.rect.y += dy
+
+    def make_hit(self):
+        self.hit = True
+        self.hit_count = 0
 
     def move_left(self, vel):
         self.x_vel = -vel
@@ -95,6 +101,12 @@ class Player(pygame.sprite.Sprite):
         self.y_vel += min(1, (self.fall_count/fps) * self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
 
+        if self.hit:
+            self.hit_count += 1
+        if self.hit_count > fps*2:
+            self.hit = False
+            self.hit_count = 0
+
         self.fall_count += 1
         self.update_sprite()
 
@@ -109,7 +121,9 @@ class Player(pygame.sprite.Sprite):
 
     def update_sprite(self):
         sprite_sheet = "idle"
-        if self.y_vel < 0:
+        if self.hit:
+            sprite_sheet = "hit"
+        elif self.y_vel < 0:
             if self.jump_count == 1:
                 sprite_sheet = "jump"
             elif self.jump_count == 2:
@@ -214,8 +228,9 @@ def handle_vertical_collision(player, objects, dy):
             elif dy<0:
                 player.rect.top = obj.rect.bottom
                 player.hit_head()
-            
-        collided_objects.append(obj)
+                    
+            collided_objects.append(obj)
+
     return collided_objects
 
 def collide(player, objects, dx):
@@ -245,7 +260,11 @@ def handle_movement(player, objects):
     if keys[pygame.K_RIGHT] and not collide_right:
         player.move_right(PLAYER_VEL)
 
-    handle_vertical_collision(player, objects, player.y_vel)
+    vertical_collide = handle_vertical_collision(player, objects, player.y_vel)
+    to_check = [collide_left, collide_right, *vertical_collide]
+    for obj in to_check:
+        if obj and obj.name == "fire":
+            player.make_hit()
 
 def main(screen):
     clock = pygame.time.Clock()
